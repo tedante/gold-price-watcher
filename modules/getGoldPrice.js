ecaeba3975680b673c0726cc65050f1a9aaded6b
Teddy K <teddykoerniadi@gmail.com>
2023-04-29 09:54:42 +0700

    test(getGoldPrice.js): add test for formatToInteger function feat(getGoldPrice.js): add formatToInteger function to format gold price string into integer

--- modules/getGoldPrice.js
+++ modules/getGoldPrice.js
@@ -11,18 +11,27 @@ dayjs.extend(timezone)
 export const getGoldPrice = async (requestBody) => {
   const axiosResponse = await axios.request({
     method: "GET",
-    // url: "https://www.tokopedia.com/emas/harga-hari-ini/",
-    url: "https://harga-emas.org/",
+    url: "https://pluang.com/widgets/price-graph/desktop-vertical",
     headers: {
       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
     }
   })
-  const $ = cheerio.load(axiosResponse.data)
-  // const htmlElement = $("#gold-price").text()
-  console.log($('#container > div:nth-child(2) > div > table > tbody > tr:nth-child(4) > td:nth-child(10)').text(), ">>>>?");
-  // console.log($.html(), ">>>>?");
 
-  return "200000"
+  const $ = cheerio.load(axiosResponse.data)
+
+  let price = $('#gold-price').text()
+
+  return price
 }
 
-export default getGoldPrice
+export const formatToInteger = (value) => {
+  let result = 0
+
+  result = value.replaceAll('Rp', '')
+  result = result.replaceAll('/g', '')
+  result = result.replaceAll('.', '')
+
+  result = Number(result)
+
+  return result
+}
