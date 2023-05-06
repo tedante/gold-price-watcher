8366318ba15e0d6673466a2faa06cec0020e070f
Teddy K <teddykoerniadi@gmail.com>
2023-05-06 17:29:11 +0700

    test checkGoldPrice

--- tests/goldPrice.test.js
+++ tests/goldPrice.test.js
@@ -1,10 +1,32 @@
-import { goldPrice } from "#modules/goldPrice";
+import { getGoldPrice, formatToInteger } from "#modules/getGoldPrice";
+import { checkGoldPrice } from "#modules/checkGoldPrice";
 import { expect, test } from '@jest/globals';
 
-describe('goldPrice module', () => {
+describe('getGoldPrice module', () => {
+  test(`should have a integer value`, async () => {
+    let value = "Rp1.012.344/g"
+    let data = formatToInteger(value)
+
+    expect(data).toBeGreaterThanOrEqual(0);
+  });
+
   test(`should have a truthy value`, async () => {
-    let data = await goldPrice()
+    let data = await getGoldPrice()
 
     expect(data).toBeTruthy();
+  });
+});
+
+describe('checkGoldPrice module', () => {
+  test(`should have a false status`, async () => {
+    let data = checkGoldPrice(1_000_000, 900_000)
+
+    expect(data.status).toBeFalsy();
+  });
+
+  test(`should have a true status`, async () => {
+    let data = checkGoldPrice(800_000, 900_000)
+
+    expect(data.status).toBeTruthy();
   });
 });
